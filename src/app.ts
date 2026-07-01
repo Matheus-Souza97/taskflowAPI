b8c439ad3035d2ea5a5b1747fd0655c593315f6d
Matheus-Souza97 <matheus-souza1@outlook.com.br>
2026-07-01 00:18:56 -0300

    feat: create test controller and router to validate server

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express"
 import { errorHandling } from "./utils/error-handling"
+import { routes } from "./routes"
 
 const app = express()
 
 app.use(express.json())
+app.use(routes)
 app.use(errorHandling)
+
 
 
 export { app }
