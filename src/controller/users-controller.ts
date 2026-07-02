64c0815d17fd17ccb789a14de54571914223116b
Matheus-Souza97 <matheus-souza1@outlook.com.br>
2026-07-02 07:44:07 -0300

    feat(users): hash password before storing user

--- src/controller/users-controller.ts
+++ src/controller/users-controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from "express";
-
+import { hash } from "bcrypt"
 import { z } from "zod"
 
 class UserController {
-  create(request:Request, response:Response, next:NextFunction) {
+  async create(request:Request, response:Response, next:NextFunction) {
     const bodySchema = z.object({
       name: z.string().trim().min(3, "O nome deve conter no minimo 3 caracteres").max(100, "O nome deve conter no maximo 100 caracteres"),
       email: z.string().email("E-mail invalido").max(150),
@@ -11,6 +11,8 @@ class UserController {
     })
 
     const { name, email, password } = bodySchema.parse(request.body)
+
+    const hashedPassword = await hash(password, 8)
 
     return response.json({message: "ok"})
   }
