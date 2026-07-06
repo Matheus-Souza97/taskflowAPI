eaf6113fd8525a16a7647e4a4f5b01fbe75a5a55
Matheus-Souza97 <matheus-souza1@outlook.com.br>
2026-07-06 11:39:41 -0300

    feat: add authentication to task history

--- src/routes/task-history-routes.ts
+++ src/routes/task-history-routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { TaskHistoryController } from "@/controller/task-history-controller";
+import { ensureAuthenticated } from "@/middleware/ensure-authenticated";
 
 const taskHistoryRoutes = Router()
 const taskHistoryController = new TaskHistoryController()
+
+taskHistoryRoutes.use(ensureAuthenticated)
 
 taskHistoryRoutes.use("/:taskId", taskHistoryController.show)
 
